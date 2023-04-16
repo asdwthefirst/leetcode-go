9554bf338835438e30c61a12f55299fba7063050
江茜 <jiangxi@highlight.mobi>
2023-04-16 20:05:34 +0800

    add cancelctx

---
+++ golearn/goconcurrencygeek/cancelctx.go
@@ -0,0 +1,41 @@
+/**
+  @author: jiangxi
+  @since: 2023/4/16
+  @desc: //TODO
+**/
+package goconcurrencygeek
+
+
+import (
+"context"
+"fmt"
+)
+
+func main() {
+	gen := func(ctx context.Context) &lt;-chan int {
+		dst := make(chan int)
+		n := 1
+		go func() {
+		for {
+		select {
+	case &lt;-ctx.Done():
+		return // returning not to leak the goroutine
+	case dst &lt;- n:
+		n++
+	}
+	}
+	}()
+		return dst
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel() // cancel when we are finished consuming integers
+
+	for n := range gen(ctx) {
+		fmt.Println(n)
+		if n == 5 {
+			break
+		}
+	}
+}
+

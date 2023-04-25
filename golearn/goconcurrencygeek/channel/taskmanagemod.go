fc094a39ecfa14bc4410c0d8fcd6d4c94f33a3ff
江茜 <jiangxi@highlight.mobi>
2023-04-25 23:42:37 +0800

    addmore

---
+++ golearn/goconcurrencygeek/channel/taskmanagemod.go
@@ -0,0 +1,303 @@
+/*
+*
+
+	@author: jiangxi
+	@since: 2023/4/24
+	@desc: //TODO
+
+*
+*/
+package channel
+
+import (
+	"fmt"
+	"reflect"
+	"time"
+)
+
+// or-Done模式，如果有多个任务，只要有任意一个任务执行完，我们就想获得这个信号
+
+// 1.递归实现
+func or(channels ...<-chan interface{}) <-chan interface{} {
+	// 特殊情况，只有零个或者1个chan
+	switch len(channels) {
+	case 0:
+		return nil
+	case 1:
+		return channels[0]
+	}
+
+	orDone := make(chan interface{})
+	go func() {
+		defer close(orDone)
+
+		switch len(channels) {
+		case 2: // 2个也是一种特殊情况
+			select {
+			case <-channels[0]:
+			case <-channels[1]:
+			}
+		default: //超过两个，二分法递归处理
+			m := len(channels) / 2
+			select {
+			case <-or(channels[:m]...):
+			case <-or(channels[m:]...):
+			}
+		}
+	}()
+
+	return orDone
+}
+
+//2.反射实现
+
+func or2(channels ...<-chan interface{}) <-chan interface{} {
+	//特殊情况，只有0个或者1个
+	switch len(channels) {
+	case 0:
+		return nil
+	case 1:
+		return channels[0]
+	}
+
+	orDone := make(chan interface{})
+	go func() {
+		defer close(orDone)
+		// 利用反射构建SelectCase
+		var cases []reflect.SelectCase
+		for _, c := range channels {
+			cases = append(cases, reflect.SelectCase{
+				Dir:  reflect.SelectRecv,
+				Chan: reflect.ValueOf(c),
+			})
+		}
+
+		// 随机选择一个可用的case
+		reflect.Select(cases)
+	}()
+
+	return orDone
+}
+
+func sig(after time.Duration) <-chan interface{} {
+	c := make(chan interface{})
+	go func() {
+		defer close(c)
+		time.Sleep(after)
+	}()
+	return c
+}
+
+func mainorDone() {
+	start := time.Now()
+
+	<-or(
+		sig(10*time.Second),
+		sig(20*time.Second),
+		sig(30*time.Second),
+		sig(40*time.Second),
+		sig(50*time.Second),
+		sig(01*time.Minute),
+	)
+
+	fmt.Printf("done after %v", time.Since(start))
+}
+
+//fanin模式，多个输入channel，一个输出channel
+//1.反射实现
+
+func fanInReflect(chans ...<-chan interface{}) <-chan interface{} {
+	out := make(chan interface{})
+	go func() {
+		defer close(out)
+		// 构造SelectCase slice
+		var cases []reflect.SelectCase
+		for _, c := range chans {
+			cases = append(cases, reflect.SelectCase{
+				Dir:  reflect.SelectRecv,
+				Chan: reflect.ValueOf(c),
+			})
+		}
+
+		// 循环，从cases中选择一个可用的
+		for len(cases) > 0 {
+			i, v, ok := reflect.Select(cases)
+			if !ok { // 此channel已经close
+				cases = append(cases[:i], cases[i+1:]...)
+				continue
+			}
+			out <- v.Interface()
+		}
+	}()
+	return out
+}
+
+//2.递归模式
+
+func fanInRec(chans ...<-chan interface{}) <-chan interface{} {
+	switch len(chans) {
+	case 0:
+		c := make(chan interface{})
+		close(c)
+		return c
+	case 1:
+		return chans[0]
+	case 2:
+		return mergeTwo(chans[0], chans[1])
+	default:
+		m := len(chans) / 2
+		return mergeTwo(
+			fanInRec(chans[:m]...),
+			fanInRec(chans[m:]...))
+	}
+}
+
+//fanin模式的特例，合并两个channel
+
+func mergeTwo(a, b <-chan interface{}) <-chan interface{} {
+	c := make(chan interface{})
+	go func() {
+		defer close(c)
+		for a != nil || b != nil { //只要还有可读的chan
+			select {
+			case v, ok := <-a:
+				if !ok { // a 已关闭，设置为nil
+					a = nil
+					continue
+				}
+				c <- v
+			case v, ok := <-b:
+				if !ok { // b 已关闭，设置为nil
+					b = nil
+					continue
+				}
+				c <- v
+			}
+		}
+	}()
+	return c
+}
+
+//fanOut
+
+func fanOut(ch <-chan interface{}, out []chan interface{}, async bool) {
+	go func() {
+		defer func() { //退出时关闭所有的输出chan
+			for i := 0; i < len(out); i++ {
+				close(out[i])
+			}
+		}()
+
+		for v := range ch { // 从输入chan中读取数据
+			v := v
+			for i := 0; i < len(out); i++ {
+				i := i
+				if async { //异步
+					go func() {
+						out[i] <- v // 放入到输出chan中,异步方式
+					}()
+				} else {
+					out[i] <- v // 放入到输出chan中，同步方式
+				}
+			}
+		}
+	}()
+}
+
+//Stream模式，把 Channel 当作流式管道使用的方式，也就是把 Channel 看作流（Stream），
+//提供跳过几个元素，或者是只取其中的几个元素等方法。
+
+func asStream(done <-chan struct{}, values ...interface{}) <-chan interface{} {
+	s := make(chan interface{}) //创建一个unbuffered的channel
+	go func() {                 // 启动一个goroutine，往s中塞数据
+		defer close(s)             // 退出时关闭chan
+		for _, v := range values { // 遍历数组
+			select {
+			case <-done:
+				return
+			case s <- v: // 将数组元素塞入到chan中
+			}
+		}
+	}()
+	return s
+}
+func takeN(done <-chan struct{}, valueStream <-chan interface{}, num int) <-chan interface{} {
+	takeStream := make(chan interface{}) // 创建输出流
+	go func() {
+		defer close(takeStream)
+		for i := 0; i < num; i++ { // 只读取前num个元素
+			select {
+			case <-done:
+				return
+			case takeStream <- <-valueStream: //从输入流中读取元素
+			}
+		}
+	}()
+	return takeStream
+}
+
+//mapreduce
+
+func mapChan(in <-chan interface{}, fn func(interface{}) interface{}) <-chan interface{} {
+	out := make(chan interface{}) //创建一个输出chan
+	if in == nil {                // 异常检查
+		close(out)
+		return out
+	}
+
+	go func() { // 启动一个goroutine,实现map的主要逻辑
+		defer close(out)
+		for v := range in { // 从输入chan读取数据，执行业务操作，也就是map操作
+			out <- fn(v)
+		}
+	}()
+
+	return out
+}
+
+func reduce(in <-chan interface{}, fn func(r, v interface{}) interface{}) interface{} {
+	if in == nil { // 异常检查
+		return nil
+	}
+
+	out := <-in         // 先读取第一个元素
+	for v := range in { // 实现reduce的主要逻辑
+		out = fn(out, v)
+	}
+
+	return out
+}
+
+// 生成一个数据流
+func asStreamMapReduce(done <-chan struct{}) <-chan interface{} {
+	s := make(chan interface{})
+	values := []int{1, 2, 3, 4, 5}
+	go func() {
+		defer close(s)
+		for _, v := range values { // 从数组生成
+			select {
+			case <-done:
+				return
+			case s <- v:
+			}
+		}
+	}()
+	return s
+}
+
+func mainMapReduce() {
+	in := asStream(nil)
+
+	// map操作: 乘以10
+	mapFn := func(v interface{}) interface{} {
+		return v.(int) * 10
+	}
+
+	// reduce操作: 对map的结果进行累加
+	reduceFn := func(r, v interface{}) interface{} {
+		return r.(int) + v.(int)
+	}
+
+	sum := reduce(mapChan(in, mapFn), reduceFn) //返回累加结果
+	fmt.Println(sum)
+}
